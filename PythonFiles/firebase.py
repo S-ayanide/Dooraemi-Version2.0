9b58d1dcbd6df1b96ae3718f62bf45f895dde8d9
Sayan Mondal <34975209+S-ayanide@users.noreply.github.com>
2020-03-12 07:06:49 +0530

    Update firebase.py

--- PythonFiles/firebase.py
+++ PythonFiles/firebase.py
@@ -1,6 +1,6 @@
 from firebase import firebase
  
-firebase = firebase.FirebaseApplication('https://dooraemi-35dd1.firebaseio.com/', None)
+firebase = firebase.FirebaseApplication('https://YOUR_FIREBASE_DATABSE_URL', None)
 data =  { 'Image': 'NULL',
           'TimeStamp': 'Some Timestamp'
         }
